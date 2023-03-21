b23779604798f813b43b13be6c1d9989deace48c
Makitey <weldemichaelmakda@gmail.com>
2023-03-21 12:19:41 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alpha in lower
+ */
+void print_alphabet(void)
+{
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+		_putchar(la);
+	_putchar('\n');
+	}
