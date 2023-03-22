480609ee1e407270858995a230a0d7f4311f1c9f
Makitey <weldemichaelmakda@gmail.com>
2023-03-22 11:08:27 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,14 +4,15 @@
  *description: print 10x
  *Return: 0 (success)
  */
+
 void print_alphabet_x10(void)
 {
 	int ten;
 	char la;
 
-	for (ten=0; ten<=9; ten++)
+	for (ten = 0; ten <= 9; ten++)
 	{
-		for (la='a'; la<='z'; la++)
+		for (la = 'a'; la <= 'z'; la++)
 			_putchar(la);
 		_putchar('\n');
 	}
