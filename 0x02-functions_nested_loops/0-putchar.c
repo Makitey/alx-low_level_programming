369703641187071f02f9c25827ca26dd3add4c46
Makitey <weldemichaelmakda@gmail.com>
2023-03-22 10:33:53 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,32 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-#endif
-
 #include "main.h"
-#include <unistd.h>
-/**
- * _putchar - used to write char "d" to stdot
- * @c: The character to be print
- * Return: 1 in success & error on -1
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-	}
-
 /**
  * main - Entry point _putchar
  * description: print out
@@ -44,4 +16,4 @@ int main(void)
 	_putchar('r');
 	_putchar('\n');
 	return (0);
-	}
+}
