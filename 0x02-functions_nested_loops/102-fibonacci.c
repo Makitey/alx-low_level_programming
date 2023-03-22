0b7508708cec6a85eb12f7d6524ecdb9ac643baa
Makitey <weldemichaelmakda@gmail.com>
2023-03-22 12:25:59 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main -main block
+ * Description: computer and prints even number <4,000,000
+ * 5 below 1024 (exclude), followed by a new l
+ */
+
+int main(void)
+{
+	int sum = 0;
+	int a = 0, b = 1, next = 0;
+	
+	while (next < 4000000)
+	{
+		next = a + b;
+		a = b;
+		b = next;
+		if (next % 2 == 0)
+			sum += next;
+	}
+	printf("%i\n", sum);
+	return (0);
+}
