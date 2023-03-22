b7f201920db1bb039745b0f52c4201e7a0474d1e
Makitey <weldemichaelmakda@gmail.com>
2023-03-22 15:54:41 -0700

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
+
 /**
- * main - main block
- * Description: computer and prints even number <4,000,000
- * 5 below 1024 (exclude), followed by a new 1
+ * main - Prints the sum of even valued
+ *
+ * Return: Always (0).
  */
 
 int main(void)
