f3f689d7a7ae90213e29fe987981029979902d88
Makitey <weldemichaelmakda@gmail.com>
2023-03-23 09:06:06 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <stdio.h>
 
 /**
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,3 +9,13 @@
  * @year: year
  * Return: void
  */
+
+void print_remaining_days(int month, int day, int year)
+{
+	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
+	{
+		if (month >= 2 && day >= 60)
+		{
+			day++;
+		}
+		printf("Day of the year: %d\n", day);
