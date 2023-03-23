40c32275156c7eaa91f69f97a318b486ab0321a0
Makitey <weldemichaelmakda@gmail.com>
2023-03-23 08:27:30 -0700

    2-largest_number.c

--- 0x03-debugging/2-main.c
+++ 0x03-debugging/2-main.c
@@ -7,12 +7,12 @@
  */
 int main(void)
 {
-	int a, b, c, la;
+	int a, b, c, largest;
 
 	a = 972;
 	b = -98;
 	c = 0;
-	la = largest_number(a, b, c);
-	printf("%d is the largest number\n", la);
+	largest = largest_number(a, b, c);
+	printf("%d is the largest number\n", largest);
 	return (0);
 }
