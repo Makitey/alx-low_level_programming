fe65bcaaa05d22a2b1857b4e6c86a29e2735d3e1
Makitey <weldemichaelmakda@gmail.com>
2023-03-30 10:56:39 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -10,5 +10,10 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 int _putchar(char);
+char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
 
 #endif
+
