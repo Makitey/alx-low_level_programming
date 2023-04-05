00d4fb1a1ef1f54f5d66eca87a2929829eace8fa
Makitey <weldemichaelmakda@gmail.com>
2023-04-05 10:58:34 -0700

    1-print_rev_recursion.c

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,16 +1,15 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - size
+ * _print_rev_recursion - reverse
  * @s: pointer to string params
- * Return: recursion
  */
 
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
-	if (!*s)
+	if (*s)
 	{
-		return (0);
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	return (1 + _strlen_recursion(++s));
 }
