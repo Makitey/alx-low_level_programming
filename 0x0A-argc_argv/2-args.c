5d553108c7ba69499827e4d8fa6adeb2198318e2
Makitey <weldemichaelmakda@gmail.com>
2023-04-07 12:34:35 -0700

    3-mul.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,6 +11,6 @@ int main(int argc, char *argv[])
 	int i;
 
 	for (i = 0; i < argc; i++)
-		printf("%s\n", argv[]);
+		printf("%s\n", argv[i]);
 	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	if (argc != 3)
 	{
-		printf(Error\n);
+		printf("Error\n");
 		return (1);
 	}
 	n1 = atoi(argv[1]);
