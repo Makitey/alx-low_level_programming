c040f98bc399357795e11eeae132ee5525d750d3
Makitey <weldemichaelmakda@gmail.com>
2023-04-07 12:42:31 -0700

    3-mul.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -9,6 +9,6 @@
 
 int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	prinf("%d\n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * main - prints the multiplication of two numbers followed by a new line
