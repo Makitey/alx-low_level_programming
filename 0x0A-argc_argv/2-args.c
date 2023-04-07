5db50a0617f9ac64af226d507b5d07e7eacaea0a
Makitey <weldemichaelmakda@gmail.com>
2023-04-07 12:26:31 -0700

    2-args.c

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -4,7 +4,7 @@
  * main - prints all arguments it receives
  * @argc: the number of argumments in the program
  * @argv: an array of arguments
- * return: always 0
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+/**
+ * main - prints the multiplication of two numbers followed by a new line
+ * @argc: the number of arrguments in the program
+ * @argv: an array of arguments
+ * Return: if the program receives two arguments -0
+ */
+
+int main(int argc, char *argv[])
+{
+	int n1, n2, prod;
+
+	if (argc != 3)
+	{
+		printf(Error\n);
+		return (1);
+	}
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	prod = n1 * n2;
+
+	printf("%d\n", prod);
+	return (0);
+}
