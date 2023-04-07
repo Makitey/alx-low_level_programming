23f193db8508e926bd75f88cd1e0ec7d785c9da1
Makitey <weldemichaelmakda@gmail.com>
2023-04-07 11:26:29 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  * Return: always 0
  */
 
-int main(int argc, char *argv[])
+int main(int my_name argc, char *argv[]);
 {
 	printf("The name of the program is: %s\n", argv[0]);
 	return (0);
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+int main(int my_name argc, char *argv[]);
+int main(int argc, char numberof_arguments *argv[]);
+int main(int argc, char *argv[]);
+int _putchar(char c);
+
+#endif
