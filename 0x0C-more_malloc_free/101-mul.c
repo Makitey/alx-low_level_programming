5dae414c9f7d0eb54ea13ac979c8a36777b16824
Makitey <weldemichaelmakda@gmail.com>
2023-04-12 11:34:49 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include<stdio.h>
+#include<stdlib.h>
 
 /**
  * main - multiplies two positive numbers
@@ -14,15 +15,20 @@ unsigned long mul;
 int num1, num2;
 
 	if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
+	{ 
+		printf("Error\n");
+		exit(98);
+	}
+
 	for (num1 = 1; num1 < argc; num1++)
 	{
 		for (num2 = 0; argv[num1][num2] != '\0'; num2++)
 		{
 			if (argv[num1][num2] > 57 || argv[num1][num2] < 48)
-			{  printf("Error\n");
-			exit(98); }
+			{
+				printf("Error\n");
+				exit(98);
+			}
 		}
 
 	}
