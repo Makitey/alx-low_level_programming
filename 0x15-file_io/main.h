6a13a2ce56351545d1c950d4d725f5f9eb4dca69
Makitey <weldemichaelmakda@gmail.com>
2023-07-10 14:15:08 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -27,4 +27,5 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
+
 #endif
