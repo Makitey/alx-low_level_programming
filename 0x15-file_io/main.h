cd7f979e0e21f3b051ed9ca08f974d5e7f1311d9
Makitey <weldemichaelmakda@gmail.com>
2023-07-10 13:45:30 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,6 +12,7 @@
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
+int _putchar(char c);
 
 /**
  * ELF header functions
