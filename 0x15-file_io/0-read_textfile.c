17a234c1091eb12fe4452c1612733c76c41d8353
Makitey <weldemichaelmakda@gmail.com>
2023-07-10 13:57:11 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,37 +1,34 @@
 #include "main.h"
+
 /**
- * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
- * @letters: number of letters to read and print
+ * read_textfile - Reads the text_file and prints it into stdout display.
+ * @filename: name of a file that is to be readed.
+ * @letters: number of letters that is to read and get print.
  * Return: the number of letters printed, or 0 if it failed
  */
+ 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int i, y;
-	char *buf;
-	if (!filename)
+	int txt_file, total, read_status;
+	char buffer[BUFSIZE];
+
+	if (filename == NULL)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
+	txt_file = open(filename, O_RDONLY);
+	if (txt_file == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-	i = read(fd, buf, letters);
-	if (i < 0)
+	total = 0;
+	read_status = 1;
+	while (letters > BUFSIZE && read_status != 0)
 	{
-		free(buf);
-		return (0);
+		read_status = read(txt_file, buffer, BUFSIZE);
+		write(STDOUT_FILENO, buffer, read_status);
+		total += read_status;
+		letters -= BUFSIZE;
 	}
-	buf[i] = '\0';
-	close(fd);
-	y = write(STDOUT_FILENO, buf, i);
-	if (y < 0)
-	{
-		free(buf);
-		return (0);
-	}
-	free(buf);
-	return (y);
+	read_status = read(txt_file, buffer, letters);
+	write(STDOUT_FILENO, buffer, read_status);
+	total += read_status;
+	close(txt_file);
+	return (total);
 }
