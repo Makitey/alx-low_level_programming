51be580ef1572df8983a1acef343d5cc31d83d85
Makitey <weldemichaelmakda@gmail.com>
2023-07-10 14:13:10 -0700

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,13 +1,13 @@
 #ifndef MAIN_H
-#define MAIN_H
+#define MAIN_N
 
+#include <elf.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <elf.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -16,7 +16,7 @@ int append_text_to_file(const char *filename, char *text_content);
 
 /**
  * ELF header functions
- */
+*/
 void check_elf(unsigned char *e_ident);
 void print_magic(unsigned char *e_ident);
 void print_class(unsigned char *e_ident);
@@ -27,5 +27,4 @@ void print_osabi(unsigned char *e_ident);
 void print_type(unsigned int e_type, unsigned char *e_ident);
 void print_entry(unsigned long int e_entry, unsigned char *e_ident);
 void close_elf(int elf);
-
 #endif
