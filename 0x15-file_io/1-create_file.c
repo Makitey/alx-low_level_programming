c6914073763f6e1648692636c2bc8fadc2984c6f
Makitey <weldemichaelmakda@gmail.com>
2023-10-30 23:05:39 +0300

    update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,32 +2,32 @@
 
 /**
  * create_file - creates a file and fills it with text
- * @filename: name of a file that is to be readed.
- * @text_content: text that is to be  write in the file.
+ * @filename: name of the file to create
+ * @text_content: text to write in the file
+ *
  * Return: 1 on success, -1 on failure
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int file_def, x, y = 0;
+	int fd, x, y = 0;
 
 	if (!filename)
 		return (-1);
 
-	file_def = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (file_def < 0)
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (fd < 0)
 		return (-1);
 
 	if (text_content)
 	{
 		while (text_content[y])
 			y++;
-		x = write(file_def, text_content, y);
+		x = write(fd, text_content, y);
 		if (x != y)
 			return (-1);
 	}
 
-	close(file_def);
+	close(fd);
 
 	return (1);
 }
